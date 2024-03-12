33bb7de2c9350eceb594ca0fc517a163c4e0a1b0
ING69COOLER <159482318+ING69COOLER@users.noreply.github.com>
2024-03-12 10:17:57 -0500

    ..

--- proyecto1/src/main/java/co/edu/uniquindio/poo/Curso.java
+++ proyecto1/src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.poo;
 import java.util.LinkedList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Curso {
     private final String nombre;
@@ -37,6 +38,40 @@ public class Curso {
         }
         return existeEstudiante;
 
+    }
+    public Estudiante getEstudiante(String numeroidentificacion){
+        Estudiante estudianteencontrado = null;
+        for (Estudiante estudiante : listaEstudiantes) {
+            if (estudiante.numerodeindentificacion().equals(numeroidentificacion)) {
+                estudianteencontrado = estudiante;
+            }
+            
+        }
+        assert estudianteencontrado != null;
+        return estudianteencontrado;
+    }
+    public Collection<Estudiante> getlistaEstudiantesordenados() {
+        Collection<Estudiante>listaOrdenada = null;
+        //Realizar el metodo burbuja para ordenar una lista de objetos en java
+
+        listaOrdenada=listaEstudiantes.stream().sorted(Comparator.comparing(Estudiante::nombres)).toList();
+        return Collections.unmodifiableCollection(listaOrdenada);
+    }
+    public Collection<Estudiante> getlistaEstudiantesedadDesendente() {
+        Collection<Estudiante>listaOrdenada = null;
+        //Realizar el metodo burbuja para ordenar una lista de objetos en java
+
+        listaOrdenada=listaEstudiantes.stream().sorted(Comparator.comparing(Estudiante::edad).reversed()).toList();
+        return Collections.unmodifiableCollection(listaOrdenada);
+    }
+    public Collection<Estudiante> getlistaMayores() {
+       Collection<Estudiante> listaMayoresEdad = null;
+       for(Estudiante estudiante:listaEstudiantes){
+        if(estudiante.edad() >= 18){
+            listaMayoresEdad.add(estudiante);
+        }
+       }
+       return listaMayoresEdad;
     }
 
     }
--- proyecto1/src/test/java/co/edu/uniquindio/poo/CursoTest.java
+++ proyecto1/src/test/java/co/edu/uniquindio/poo/CursoTest.java
@@ -1,10 +1,13 @@
 package co.edu.uniquindio.poo;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collection;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 public class CursoTest {
     private static final Logger LOG =  Logger.getLogger(EstudianteTest.class.getName());
    
@@ -47,5 +50,75 @@ public class CursoTest {
         curso.registrarEstudiante(estudiante_1);
         assertThrows(Throwable.class, ()-> curso.registrarEstudiante(estudiante_1)); 
         LOG.info("Fin prueba datos respetidos");
+    }
+
+    @Test
+    public void obtenerEstudiante(){
+        LOG.info("Inicio prueba  ontener Estudiante");
+        Curso curso = new Curso ("Programacion 1");
+        Estudiante estudiante_1 = new Estudiante("Manuel", "Pineda Varela", "1092455543", "3045879300", "manueljk@sljfkl@jjfasdfl", (byte)18);
+        curso.registrarEstudiante(estudiante_1);
+        assertThrows(Throwable.class,()-> curso.getEstudiante("102455543"));
+        LOG.info("Inicio prueba  Obtener estudiante");
+
+
+    }
+    @Test
+    public void estudiantesAlfabeticos(){
+        LOG.info("Inicio prueba  estudiantesAlfabeticos");
+        Curso curso = new Curso ("Programacion 1");
+        Estudiante estudiante_1 = new Estudiante("Manuel", "Pineda Varela", "1092455543", "3045879300", "manueljk@sljfkl@jjfasdfl", (byte)18);
+        curso.registrarEstudiante(estudiante_1);
+        Estudiante estudiante = new Estudiante ("Samnuel","Pineda","10924543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante);
+        Estudiante estudiante2 = new Estudiante ("antiago","Pineda","10928455543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante2);
+
+        Collection<Estudiante>listaOrdenadaObtenida = curso.getlistaEstudiantesordenados();
+        Collection<Estudiante>listaOrdenadaDeseada = List.of(estudiante_1,estudiante,estudiante2);
+        assertIterableEquals(listaOrdenadaObtenida, listaOrdenadaDeseada);
+        for(Estudiante nombre : curso.getlistaEstudiantesordenados()){
+            System.out.println(nombre.getNombre());
+        }
+        LOG.info("Inicio prueba estudiantesAlfabeticos");
+
+    }
+    @Test
+    public void estudiantesEdadDesendente(){
+        LOG.info("Inicio prueba  estudiantesAlfabeticos");
+        Curso curso = new Curso ("Programacion 1");
+        Estudiante estudiante_1 = new Estudiante("Manuel", "Pineda Varela", "1092455543", "3045879300", "manueljk@sljfkl@jjfasdfl", (byte)18);
+        curso.registrarEstudiante(estudiante_1);
+        Estudiante estudiante = new Estudiante ("Samnuel","Pineda","10924543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante);
+        Estudiante estudiante2 = new Estudiante ("antiago","Pineda","10928455543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante2);
+
+        Collection<Estudiante>listaOrdenadaObtenida = curso.getlistaEstudiantesedadDesendente();
+        Collection<Estudiante>listaOrdenadaDeseada = List.of(estudiante,estudiante2,estudiante_1);
+        assertIterableEquals(listaOrdenadaObtenida, listaOrdenadaDeseada);
+        for(Estudiante edad : curso.getlistaEstudiantesedadDesendente()){
+            System.out.println(edad.getNombre());
+        }
+        LOG.info("Inicio prueba estudiantesAlfabeticos");
+    }
+    @Test
+    public void listaMayores(){
+        LOG.info("Inicio prueba  estudiantesAlfabeticos");
+        Curso curso = new Curso ("Programacion 1");
+        Estudiante estudiante_1 = new Estudiante("Manuel", "Pineda Varela", "1092455543", "3045879300", "manueljk@sljfkl@jjfasdfl", (byte)18);
+        curso.registrarEstudiante(estudiante_1);
+        Estudiante estudiante = new Estudiante ("Samnuel","Pineda","10924543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante);
+        Estudiante estudiante2 = new Estudiante ("antiago","Pineda","10928455543","3045879300","manuel.pinedav@uqvirtual.edu.co ",(byte)19); 
+        curso.registrarEstudiante(estudiante2);
+
+        Collection<Estudiante>listaOrdenadaObtenida = curso.getlistaMayores();
+        Collection<Estudiante>listaOrdenadaDeseada = List.of(estudiante,estudiante2,estudiante_1);
+        assertIterableEquals(listaOrdenadaObtenida, listaOrdenadaDeseada);
+        for(Estudiante edad : curso.getlistaMayores()){
+            System.out.println(edad.getNombre());
+        }
+        LOG.info("Inicio prueba estudiantesAlfabeticos");
     }
 }
